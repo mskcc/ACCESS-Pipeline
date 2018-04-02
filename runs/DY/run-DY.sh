9160ee53d2354b2fc1b0ee297c4dab6ea9a42c3c
ionox0 <ionox0@gmail.com>
2018-04-02 14:10:15 -0400

    WIP isolated standard_pipeline

--- runs/DY/run-DY.sh
+++ runs/DY/run-DY.sh
@@ -11,7 +11,7 @@ output_directory=`python -c "import os;print(os.path.abspath('${output_dir}'))"`
 
 python ../pipeline-submit.py \
     --project_name=${project} \
-    --workflow=../../workflows/innovation_pipeline.cwl \
+    --workflow=../../workflows/standard_pipeline.cwl \
     --inputs_file=inputs.yaml \
     --output_location=${output_directory} \
     --batch_system=lsf
