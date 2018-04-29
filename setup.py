285c0279ab3a0831cefe0dbea6afecda746ec912
ionox0 <ionox0@gmail.com>
2018-04-28 23:40:58 -0400

    clean command for setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from setuptools import setup
+import os
+from setuptools import setup, Command
+
+
+class CleanCommand(Command):
+    """
+    Custom clean command to tidy up the project root
+    """
+    user_options = []
+
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 
 setup(
@@ -42,5 +57,8 @@ setup(
         'python_tools/workflow_tools/QC/plotting-collapsed-bams.r'
     ],
     include_package_data = True,
-    zip_safe=False
+    zip_safe=False,
+    cmdclass={
+        'clean': CleanCommand,
+    }
 )
