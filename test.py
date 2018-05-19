766b040bdeb7c562fde9a89513377ff710724245
ionox0 <ionox0@gmail.com>
2018-05-18 23:14:12 -0400

    renaming test folders

--- test.py
+++
@@ -1,3 +0,0 @@
-from python_tools.workflow_tools.qc import qc_wrapper
-
-qc_wrapper.main()
---
+++ test/test.py
@@ -0,0 +1,4 @@
+from python_tools.workflow_tools.qc import qc_wrapper #aggregate_bam_metrics
+
+# aggregate_bam_metrics.main()
+qc_wrapper.main()
