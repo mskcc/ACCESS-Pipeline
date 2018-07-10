ad374d92aaa10d9000e5a620837ad525ed520af1
ionox0 <ionox0@gmail.com>
2018-07-09 22:54:48 -0400

    rename main_qc file

--- python_tools/workflow_tools/qc/combine_qc_pdfs.py
+++ python_tools/workflow_tools/qc/combine_qc_pdfs.py
@@ -1,16 +1,24 @@
-import sys
+import argparse
+import datetime
+import pandas as pd
 from PyPDF2 import PdfFileMerger
 
+from ...constants import *
 
-FINAL_QC_FILENAME = 'main_qc.pdf'
 
+def combine_pdfs(args):
+    date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-def combine_pdfs():
+    # Grab the pool from the title file
+    title_file = pd.read_csv(args.title_file, sep='\t')
+    pool = title_file[TITLE_FILE__POOL_COLUMN].values[0]
+
+    FINAL_QC_FILENAME = pool + '_' + date + '.pdf'
+
     merger = PdfFileMerger()
 
-    for pdf in sys.argv[1:]:
+    for pdf in args.pdf_files:
         print(pdf)
-
         merger.append(open(pdf), 'rb')
 
     with open('./' + FINAL_QC_FILENAME, 'wb') as fout:
@@ -18,7 +26,11 @@ def combine_pdfs():
 
 
 def main():
-    combine_pdfs()
+    parser = argparse.ArgumentParser(prog='Combine PDF files into one PDF', usage='%(prog)s [options]')
+    parser.add_argument('-t', '--title_file')
+    parser.add_argument('pdf_files', nargs='+', help='bar help')
+    args = parser.parse_args()
+    combine_pdfs(args)
 
 
 if __name__ == '__main__':
