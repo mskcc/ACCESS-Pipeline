2b03f3dd53cc4d5e098d55835826696c415129e5
andurill <gowtham.jn@gmail.com>
2018-08-26 22:44:54 -0400

    Add module to create title file from samplesheet

---
+++ python_tools/pipeline_kickoff/create_title_file_from_samplesheet.py
@@ -0,0 +1,80 @@
+import xlrd
+import logging
+import argparse
+import pandas as pd
+
+from ..constants import *
+
+##################################
+# Pipeline Kickoff Step #1
+#
+# This module is used to create a title file with the information needed for a pipeline run
+# It is derived from the manually-curated sample samplesheet
+#
+# Usage example:
+#
+# create_title_file_from_samplesheet \
+#   -i ./SampleSheet.csv \
+#   -o ./title_file.txt
+#
+# Note: The following requirements will be imposed on the input samplesheet file:
+#
+# 1. The fields that are found in the sample samplesheet should matched with the examples in test/test_data
+# 2. The sample ID's in the samplesheet must be matched somewhere in the fastq file names fom the -d data folder
+# 3. The sample ID's in the samplesheet must be matched somewhere in the path to the SampleSheet.csv files
+# 4. The SAMPLE_CLASS column of the samplesheet must consist of the values either "Tumor" or "Normal"
+# 5. Each "Tumor" sample must have at least one associated "Normal" sample
+# 6. Each sample folder in the -d data folder must have these three files:
+#
+# '_R1_001.fastq.gz'
+# '_R2_001.fastq.gz'
+# 'SampleSheet.csv'
+
+
+def create_title_file(samplesheet_file_path, output_filename):
+    # Read samplesheet as either csv or Excel file
+    try:
+        samplesheet = pd.read_csv(samplesheet_file_path, sep=',',skiprows=range(0,19),header=1)
+    except (xlrd.biffh.XLRDError, pd.io.common.CParserError):
+        samplesheet = pd.read_excel(samplesheet_file_path, sep=',',skiprows=range(0,19))
+    samplesheet = samplesheet.dropna(axis=0, how='all')
+    samplesheet = samplesheet.replace('\n','', regex=True)
+    
+    # Select the columns we want from the samplesheet & rename them
+    try:
+        title_file = samplesheet[columns_map_samplesheet.keys()]
+    except KeyError as e:
+        logging.error('Existing samplesheet columns:')
+        logging.error(samplesheet.columns)
+        raise e
+
+    title_file.columns = columns_map_samplesheet.values()
+
+    # Trim whitespace
+    title_file = title_file.apply(lambda x: x.str.strip() if x.dtype == 'object' else x)
+
+    # Optionally split by lanes
+    if len(title_file[TITLE_FILE__LANE_COLUMN].unique()) > 1:
+        for lane in title_file[TITLE_FILE__LANE_COLUMN].unique():
+            title_file_sub = title_file[title_file[TITLE_FILE__LANE_COLUMN] == lane]
+            # Write title file
+            # title_file_sub.to_csv('lane-{}_'.format(lane) + output_filename, sep='\t', index=False)
+            title_file_sub.to_csv(output_filename, sep='\t', index=False)
+    else:
+        title_file.to_csv(output_filename, sep='\t', index=False)
+
+########
+# Main #
+########
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--samplesheet_file_path", help="Sample Manifest File (e.g. test_samplesheet.xlsx)", required=True)
+    parser.add_argument("-o", "--output_filename", help="Desired output title location and name", required=True)
+
+    args = parser.parse_args()
+    create_title_file(args.samplesheet_file_path, args.output_filename)
+
+
+if __name__ == "__main__":
+    main()
