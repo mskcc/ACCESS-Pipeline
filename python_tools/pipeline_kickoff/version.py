491fa6c596cc3583f4decf5a3ed5bb4c294603fa
ionox0 <ionox0@gmail.com>
2018-08-28 11:19:20 -0400

    remove cached version.py file from git

--- python_tools/pipeline_kickoff/version.py
+++
@@ -1,5 +0,0 @@
-most_recent_tag = '0.0.19-41-g6efb581'
-dirty = True
-version = '6efb581838d049859314681fa8c4f5a2120ef345-dirty'
-short_version = '6efb581-dirty'
-current_commit = '6efb581838d049859314681fa8c4f5a2120ef345'
