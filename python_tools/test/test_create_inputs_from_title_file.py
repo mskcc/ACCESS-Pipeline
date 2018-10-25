79c00ea45c90ca204b6bde971b77c1c2e68ebe9c
ionox0 <ionox0@gmail.com>
2018-10-25 13:08:05 -0400

    test for multiple sample ID matches when creating inputs

--- python_tools/test/test_create_inputs_from_title_file.py
+++ python_tools/test/test_create_inputs_from_title_file.py
@@ -9,6 +9,9 @@ def load_bad_title_file():
     title_file = pd.read_csv('test_data/bad_title_file.txt', sep='\t')
     return title_file
 
+def load_good_title_file_similar_sample_names():
+    title_file = pd.read_csv('test_data/good_title_file_similar_sample_names.txt', sep='\t')
+    return title_file
 
 def load_good_title_file():
     title_file = pd.read_csv('test_data/good_title_file.txt', sep='\t')
@@ -69,6 +72,55 @@ class Tests(unittest.TestCase):
             {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_3_test_investigator_sample_6_T/test_patient_3_test_investigator_sample_6_T_R1_001.fastq.gz'},
         ]
 
+    def test_two_sample_ids_found_in_fastq(self):
+
+        fastq_objects = [
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1a/test_patient_1_test_investigator_sample_1a_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1/test_patient_1_test_investigator_sample_1_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_4_T/test_patient_2_test_investigator_sample_4_T_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_3_N/test_patient_2_test_investigator_sample_3_N_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_3_test_investigator_sample_6_T/test_patient_3_test_investigator_sample_6_T_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_5_N/test_patient_2_test_investigator_sample_5_N_R1_001.fastq.gz'}
+        ]
+
+        fastq2_objects = [
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1a/test_patient_1_test_investigator_sample_1a_R2_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1/test_patient_1_test_investigator_sample_1_R2_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_4_T/test_patient_2_test_investigator_sample_4_T_R2_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_3_test_investigator_sample_6_T/test_patient_3_test_investigator_sample_6_T_R2_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_3_N/test_patient_2_test_investigator_sample_3_N_R2_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_5_N/test_patient_2_test_investigator_sample_5_N_R2_001.fastq.gz'}
+        ]
+
+        sample_sheets = [
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1a/SampleSheet.csv'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_1/SampleSheet.csv'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1_N/SampleSheet.csv'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_4_T/SampleSheet.csv'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_3_test_investigator_sample_6_T/SampleSheet.csv'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_5_N/SampleSheet.csv'}
+        ]
+
+        title_file = load_good_title_file_similar_sample_names()
+
+        fastq1, fastq2, sample_sheet = create_inputs_from_title_file.sort_fastqs(
+            fastq_objects,
+            fastq2_objects,
+            sample_sheets,
+            title_file
+        )
+
+        self.assertListEqual(fastq1, [
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1/test_patient_1_test_investigator_sample_1_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_1_test_investigator_sample_1a/test_patient_1_test_investigator_sample_1a_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_4_T/test_patient_2_test_investigator_sample_4_T_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_3_N/test_patient_2_test_investigator_sample_3_N_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_2_test_investigator_sample_5_N/test_patient_2_test_investigator_sample_5_N_R1_001.fastq.gz'},
+            {'class': 'File', 'path': '../../test/test_data/umi-T_N-PanCancer/test_patient_3_test_investigator_sample_6_T/test_patient_3_test_investigator_sample_6_T_R1_001.fastq.gz'},
+        ])
+
+
+
     def test_validate_title_file(self):
         """
 
@@ -81,7 +133,7 @@ class Tests(unittest.TestCase):
         self.bad_title_file.loc[self.bad_title_file.index[-1], MANIFEST__BARCODE_ID_COLUMN] = 'bc411-bc411'
 
         with self.assertRaises(Exception):
-            create_inputs_from_title_file.perform_validation(self.bad_title_file)
+            create_inputs_from_title_file.perform_validation(self.bad_title_file, 'test_project_title_file.txt', 'test_project')
 
         # Fix misspelled sample class
         self.bad_title_file[MANIFEST__SAMPLE_CLASS_COLUMN] = self.bad_title_file[MANIFEST__SAMPLE_CLASS_COLUMN].str.replace('Tumore', 'Tumor')
@@ -93,7 +145,7 @@ class Tests(unittest.TestCase):
         self.bad_title_file[MANIFEST__SAMPLE_TYPE_COLUMN] = self.bad_title_file[MANIFEST__SAMPLE_TYPE_COLUMN].str.replace('Plasmaa', 'Plasma')
 
         # Now it should pass
-        create_inputs_from_title_file.perform_validation(self.bad_title_file)
+        create_inputs_from_title_file.perform_validation(self.bad_title_file, 'test_project_title_file.txt', 'test_project')
 
 
     def test_barcodes_check(self):
