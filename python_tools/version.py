9cf0b451de3ed8ec94c5e25bd26191a3323ccc4e
ionox0 <ionox0@gmail.com>
2019-01-26 09:41:42 -0500

    accidentally committed version info file to git

--- python_tools/version.py
+++
@@ -1,5 +0,0 @@
-most_recent_tag = '0.1.0-rc2'
-dirty = True
-version = '2e20b5dd3dc7a7e6d454bc9d3aa3f70aecbb7a32-dirty'
-short_version = '2e20b5d-dirty'
-current_commit = '2e20b5dd3dc7a7e6d454bc9d3aa3f70aecbb7a32'
