e8126ee0cdb9f6afa61238ff68a74b4d9e594a68
andurill <gowtham.jn@gmail.com>
2019-03-21 20:31:51 -0400

    add a check for environment

--- setup.py
+++ setup.py
@@ -1,7 +1,45 @@
 import os
+import sys
 from setuptools import setup, Command
+from setuptools.command.install import install
 
 import version
+
+while True and len(sys.argv) >= 2 and sys.argv[1] == "install":
+    try:
+        prefix = sys.prefix
+        user_input = raw_input("Current python environment is: {}\nProceed with setup install? (y/n) ".format(prefix))
+        if user_input == "n":
+            sys.exit()
+        elif user_input == "y":
+            break
+        else:
+            print "Incorrect input."
+    except (AttributeError, KeyboardInterrupt):
+        # if sys.prefix cannot be determined.
+        raise
+
+
+class install_access(install):
+    """
+    Override the standard install
+    """
+    user_options = [
+        ('keep-setup-files=', None, 'keep setup install output files')
+    ]
+    
+    def initialize_options(self):
+        self.keep_setup_files = False
+
+    def finalize_options(self):
+        if self.keep_setup_files is None:
+            pass
+
+    def run(self):
+        install.run(self)
+    
+    if True:
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 
 class CleanCommand(Command):
@@ -18,19 +56,21 @@ class CleanCommand(Command):
         os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 
-# Write version info to version.py on setup
-version_number = version.expand_()
-with open('python_tools/pipeline_kickoff/version.py', 'wb') as f:
-    f.write(version_number)
+def set_version_and_root():
+    # Write version info to version.py on setup
+    version_number = version.expand_()
+    with open('python_tools/pipeline_kickoff/version.py', 'wb') as f:
+        f.write(version_number)
 
-# Todo: need to come up with a better way to retain version info
-with open('python_tools/version.py', 'wb') as f:
-    f.write(version_number)
+    # Todo: need to come up with a better way to retain version info
+    with open('python_tools/version.py', 'wb') as f:
+        f.write(version_number)
 
-# Write current path as path to project root
-# (used to include references to the static resource files in inputs.yaml)
-with open('python_tools/root.py', 'wb') as f:
-    f.write('ROOT_DIR = ' + '\'' + os.getcwd() + '\'')
+    # Write current path as path to project root
+    # (used to include references to the static resource files in inputs.yaml)
+    with open('python_tools/root.py', 'wb') as f:
+        f.write('ROOT_DIR = ' + '\'' + os.getcwd() + '\'')
+
 
 def req_file(filename):
     """
@@ -44,63 +84,68 @@ def req_file(filename):
 
     return [x.strip() for x in content]
 
-setup(
-    name='access_pipeline',
-    version=version.most_recent_tag(),
-    description='MSKCC Center for Molecular Oncology, Innovation Lab, cfDNA sequencing pipeline',
-    url='http://github.com/mskcc/ACCESS-Pipeline',
-    author='Ian Johnson',
-    author_email='johnsoni@mskcc.org',
-    license='MIT',
-    install_requires=req_file('requirements.txt'),
-    classifiers=[
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Programming Language :: Python :: 2.7',
-    ],
-    packages = [
-        'python_tools',
-        'python_tools.pipeline_kickoff',
-        'python_tools.workflow_tools',
-        'python_tools.workflow_tools.qc',
-        'python_tools.test',
-    ],
-    package_data = {'': ['**/*.r', '**/*.R', '**/**/*.r', '**/**/*.R']},
-    entry_points = {
-        'console_scripts': [
-            # Pipeline Kickoff
-            'create_inputs_from_title_file = python_tools.pipeline_kickoff.create_inputs_from_title_file:main',
-            'create_standard_bam_to_collapsed_qc_inputs = python_tools.pipeline_kickoff.create_standard_bam_to_collapsed_qc_inputs:main',
-            'create_title_file_from_manifest = python_tools.pipeline_kickoff.create_title_file_from_manifest:main',
-            'create_title_file_from_samplesheet = python_tools.pipeline_kickoff.create_title_file_from_samplesheet:main',
-            'pipeline_submit = python_tools.pipeline_kickoff.pipeline_submit:main',
-            'pipeline_runner = python_tools.pipeline_kickoff.pipeline_runner:main',
-            # Workflow Tools
-            'list2bed = python_tools.workflow_tools.list2bed:main',
-            # Quality Control
-            'qc_wrapper = python_tools.workflow_tools.qc.qc_wrapper:main',
-            'tables_module = python_tools.workflow_tools.qc.tables_module:main',
-            'plot_noise = python_tools.workflow_tools.qc.plot_noise:main',
-            'fingerprinting = python_tools.workflow_tools.qc.fingerprinting:main',
-            'combine_qc_pdfs = python_tools.workflow_tools.qc.combine_qc_pdfs:main',
-            'gender_check = python_tools.workflow_tools.qc.gender_check:main',
-            # Postprocessing
-            'pipeline_postprocessing = python_tools.workflow_tools.pipeline_postprocessing:main',
-            # Test Utilities
-            'test_outputs = python_tools.test.test_pipeline_outputs:main',
-        ]
-    },
-    scripts=[
-        'python_tools/workflow_tools/qc/r_tools/plots_module.r',
-        'python_tools/workflow_tools/qc/r_tools/plots.r',
-        'python_tools/workflow_tools/qc/r_tools/util.r',
-        'python_tools/workflow_tools/qc/r_tools/constants.r',
-        'python_tools/workflow_tools/qc/calculate_noise.sh',
-        'python_tools/workflow_tools/qc/make_umi_qc_tables.sh',
-        'python_tools/workflow_tools/qc/aggregate_bam_metrics.sh',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    cmdclass={
-        'clean': CleanCommand,
-    }
-)
+
+def start_setup():
+    setup(
+        name='access_pipeline',
+        version=version.most_recent_tag(),
+        description='MSKCC Center for Molecular Oncology, Innovation Lab, cfDNA sequencing pipeline',
+        url='http://github.com/mskcc/ACCESS-Pipeline',
+        author='Ian Johnson',
+        author_email='johnsoni@mskcc.org',
+        license='MIT',
+        install_requires=req_file('requirements.txt'),
+        classifiers=[
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'Programming Language :: Python :: 2.7',
+        ],
+        packages = [
+            'python_tools',
+            'python_tools.pipeline_kickoff',
+            'python_tools.workflow_tools',
+            'python_tools.workflow_tools.qc',
+            'python_tools.test',
+        ],
+        package_data = {'': ['**/*.r', '**/*.R', '**/**/*.r', '**/**/*.R']},
+        entry_points = {
+            'console_scripts': [
+                # Pipeline Kickoff
+                'create_inputs_from_title_file = python_tools.pipeline_kickoff.create_inputs_from_title_file:main',
+                'create_standard_bam_to_collapsed_qc_inputs = python_tools.pipeline_kickoff.create_standard_bam_to_collapsed_qc_inputs:main',
+                'create_title_file_from_manifest = python_tools.pipeline_kickoff.create_title_file_from_manifest:main',
+                'create_title_file_from_samplesheet = python_tools.pipeline_kickoff.create_title_file_from_samplesheet:main',
+                'pipeline_submit = python_tools.pipeline_kickoff.pipeline_submit:main',
+                'pipeline_runner = python_tools.pipeline_kickoff.pipeline_runner:main',
+                # Workflow Tools
+                'list2bed = python_tools.workflow_tools.list2bed:main',
+                # Quality Control
+                'qc_wrapper = python_tools.workflow_tools.qc.qc_wrapper:main',
+                'tables_module = python_tools.workflow_tools.qc.tables_module:main',
+                'plot_noise = python_tools.workflow_tools.qc.plot_noise:main',
+                'fingerprinting = python_tools.workflow_tools.qc.fingerprinting:main',
+                'combine_qc_pdfs = python_tools.workflow_tools.qc.combine_qc_pdfs:main',
+                'gender_check = python_tools.workflow_tools.qc.gender_check:main',
+                # Postprocessing
+                'pipeline_postprocessing = python_tools.workflow_tools.pipeline_postprocessing:main',
+                # Test Utilities
+                'test_outputs = python_tools.test.test_pipeline_outputs:main',
+            ]
+        },
+        scripts=[
+            'python_tools/workflow_tools/qc/r_tools/plots_module.r',
+            'python_tools/workflow_tools/qc/r_tools/plots.r',
+            'python_tools/workflow_tools/qc/r_tools/util.r',
+            'python_tools/workflow_tools/qc/r_tools/constants.r',
+            'python_tools/workflow_tools/qc/calculate_noise.sh',
+            'python_tools/workflow_tools/qc/make_umi_qc_tables.sh',
+            'python_tools/workflow_tools/qc/aggregate_bam_metrics.sh',
+        ],
+        include_package_data=True,
+        zip_safe=False,
+        cmdclass={
+            'clean': CleanCommand,
+            #'install': install_access,
+        }
+    )
+
+start_setup()
