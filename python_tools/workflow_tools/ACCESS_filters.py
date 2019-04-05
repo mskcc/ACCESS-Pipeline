8504619041bcfabec4aa943ae04955001059b92d
ionox0 <ionox0@gmail.com>
2019-04-05 15:21:33 -0400

    no need to append to filtered maf after ACCESS_Filters

--- python_tools/workflow_tools/ACCESS_filters.py
+++ python_tools/workflow_tools/ACCESS_filters.py
@@ -258,7 +258,7 @@ def main():
     df_post_filter = apply_filter_maf(df_pre_filter, args)
 
     output_file_name = os.path.basename(args.fillout_maf).replace('.maf', '_filtered.maf')
-    df_post_filter.to_csv(output_file_name, header=True, index=None, sep='\t', mode='a')
+    df_post_filter.to_csv(output_file_name, header=True, index=None, sep='\t')
 
 
 if __name__ == '__main__':
