d9b4f0198cfaeac231d1ad9c7f194a350d37d5b7
ionox0 <ionox0@gmail.com>
2019-06-03 19:39:46 -0400

    include run params / tools resources yaml files in setup package
    in order to reference from ROOT_DIR variable using conda

--- setup.py
+++ setup.py
@@ -63,8 +63,24 @@ setup(
         'python_tools.workflow_tools',
         'python_tools.workflow_tools.qc',
         'python_tools.test',
+        'resources'
     ],
-    package_data = {'': ['**/*.r', '**/*.R', '**/**/*.r', '**/**/*.R']},
+    package_data = {
+        '': [
+            '**/*.r',
+            '**/*.R',
+            '**/**/*.r',
+            '**/**/*.R',
+        ],
+        'resources': [
+            'resources',
+            'resources/*',
+            'resources/*/*',
+            'resources/**/*',
+            '*/*.yaml',
+            '*.yaml'
+        ]
+    },
     entry_points = {
         'console_scripts': [
             # Pipeline Kickoff
@@ -97,8 +113,8 @@ setup(
         'python_tools/workflow_tools/qc/make_umi_qc_tables.sh',
         'python_tools/workflow_tools/qc/aggregate_bam_metrics.sh',
     ],
-    include_package_data=True,
-    zip_safe=False,
+    # include_package_data=True,
+    # zip_safe=False,
     cmdclass={
         'clean': CleanCommand,
     }
