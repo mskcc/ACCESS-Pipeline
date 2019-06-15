8d7e0a7adb5e8b481e9decae673194c8a492cb49
andurill <gowtham.jn@gmail.com>
2019-06-14 21:16:25 -0400

    :bug: remove root module

--- python_tools/legacy_constants.py
+++ python_tools/legacy_constants.py
@@ -2,8 +2,8 @@ import os
 import re
 from collections import OrderedDict
 
-from root import ROOT_DIR
-
+# Repository main directory
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
 #############
 # Resources #
