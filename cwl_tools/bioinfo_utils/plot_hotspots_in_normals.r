1882cafdbaeb812ebf4beebeab7708eb672d873c
ionox0 <ionox0@gmail.com>
2019-07-03 17:57:25 -0400

    hotspots plot formatting

--- cwl_tools/bioinfo_utils/plot_hotspots_in_normals.r
+++ cwl_tools/bioinfo_utils/plot_hotspots_in_normals.r
@@ -9,6 +9,13 @@ library(gridExtra)
 INPUT_TABLE_FILENAME = 'hotspots-in-normals.txt'
 OUTPUT_PLOT_FILENAME = 'hotspots_in_normals.pdf'
 
+THEME = theme(
+  text = element_text(size=24),
+  axis.text.x = element_text(angle=45, hjust=1, face='bold'),
+  axis.text.y = element_text(face='bold'),
+  plot.margin = unit(c(.1, .1, .1, 1), 'in')
+)
+
 counts = read.table(
     INPUT_TABLE_FILENAME,
     sep = '\t',
@@ -17,7 +24,7 @@ counts = read.table(
 )
 
 if (nrow(counts) == 0) {
-    pdf(file = 'hotspots_in_normals.pdf', height=15, width=20, onefile=TRUE)
+    pdf(file = OUTPUT_PLOT_FILENAME, height=15, width=20, onefile=TRUE)
     no_hotspots_msg = textGrob(label = 'No hotspots found', gp=gpar(fontsize=22, col='black'))
     grid.draw(no_hotspots_msg)
     dev.off()
@@ -26,14 +33,12 @@ if (nrow(counts) == 0) {
 
 counts$Sample = reorder(counts$Sample, counts$SampleType=='Tumor')
 
-pdf(file = 'average_coverage_duplex_A_exon.pdf', width=11, height=8, onefile=TRUE)
-
 g = ggplot(counts, aes(Sample, Mutation)) +
     theme_bw() +
     geom_point(aes(color=SampleType, size=AF, shape=15)) +
     scale_shape_identity() +
-    geom_text(aes(label=paste(sep='', AF, ' (', AltCount, '/', Total, ')')), vjust=0, nudge_y = -0.30) +
-    ggtitle('Hotspot mutations in normals (normals: unfiltered, tumors: duplex)') +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, face='bold'), axis.text.y=element_text(face='bold'), plot.margin = unit(c(.1, .1, .1, 1), 'in'))
+    geom_text(aes(label=paste(sep='', AF, ' (', AltCount, '/', Total, ')')), vjust=0, nudge_y = -0.60, size=6) +
+    ggtitle('Hotspot mutations in normals (normals: unfiltered, tumors: duplex, > 3 fragments)') +
+    THEME
 
 ggsave(g, file=OUTPUT_PLOT_FILENAME, width=20, height=8.5)
