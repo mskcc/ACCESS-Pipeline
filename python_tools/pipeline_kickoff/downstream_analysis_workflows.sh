bbd9b3aaec2eda3f61a0b75d7a787db53b685e5f
Ian Johnson <ionox0@gmail.com>
2020-06-30 16:01:26 -0400

    kickoff script cleanup

--- python_tools/pipeline_kickoff/downstream_analysis_workflows.sh
+++ python_tools/pipeline_kickoff/downstream_analysis_workflows.sh
@@ -38,23 +38,18 @@
 # Example Usage:
 #
 # downstream_analysis_workflows.sh \
-# -P 				Project_10619_B \
-# -T 				/juno/work/access/production/runs/Project_10619_B/metadata/Project_10619_B.txt \
-# -P 				/juno/work/access/production/runs/Project_10619_B/small_variants/pairing.txt \
-# -DTB 			    /juno/work/access/production/runs/Project_10619_B/bam_qc/Project_10619_B_1.3.26/duplex_tumor_bams \
-# -STB 			    /juno/work/access/production/runs/Project_10619_B/bam_qc/Project_10619_B_1.3.26/simplex_tumor_bams \
-# -UTB 			    /juno/work/access/production/runs/Project_10619_B/copy_number_variants/unfiltered_tumor_bams \
-# -STTB 			/juno/work/access/production/runs/Project_10619_B/bam_qc/Project_10619_B_1.3.26/all_standard_bams \
-# -UNB 			    /juno/work/access/production/runs/Project_10619_B/bam_qc/Project_10619_B_1.3.26/unfiltered_normal_bams \
-# -DDN 			    /work/access/production/resources/msk-access/current/novaseq_unmatched_normal_plasma_duplex_bams_dmp/current/DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam \
-# -DSTN 			/work/access/production/resources/msk-access/current/novaseq_unmatched_normal_plasma_standard_bams_dmp/current/DONOR22-TP_cl_aln_srt_MD_IR_FX_BR.bam \
-# -CDB 			    /work/access/production/resources/msk-access/current/novaseq_curated_duplex_bams_dmp/current/ \
-# -CSB 			    /work/access/production/resources/msk-access/current/novaseq_curated_simplex_bams_dmp/current/
-
-# Todo: hard code these options?
-# /work/access/production/resources/msk-access/current/novaseq_unmatched_normal_plasma_duplex_bams_dmp/current/DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam
-# curated bam directories
-# default standard normal bams
+# -P 				    Project_10619_B \
+# -T 				    ./Project_10619_B.txt \
+# -P 				    ./pairing.txt \
+# -DTB 			    ./duplex_tumor_bams \
+# -STB 			    ./simplex_tumor_bams \
+# -UTB 			    ./unfiltered_tumor_bams \
+# -STTB 			  ./all_standard_bams \
+# -UNB 			    ./unfiltered_normal_bams \
+# -DDN 			    ./novaseq_unmatched_normal_plasma_duplex_bams_dmp/current/DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam \
+# -DSTN 			  ./novaseq_unmatched_normal_plasma_standard_bams_dmp/current/DONOR22-TP_cl_aln_srt_MD_IR_FX_BR.bam \
+# -CDB 			    ./novaseq_curated_duplex_bams_dmp/current/ \
+# -CSB 			    ./novaseq_curated_simplex_bams_dmp/current/
 
 
 ############
@@ -89,7 +84,6 @@ while (( "$#" )); do
         PAIRING_FILE_PATH=$2;;
     -T)
         TITLE_FILE_PATH=$2
-        echo $2;;
     esac
     shift
     shift
@@ -104,33 +98,33 @@ cnv_inputs=${PROJECT_ID}_cnv_inputs.yaml
 msi_inputs=microsatellite_instability/${PROJECT_ID}_msi_inputs.yaml
 
 generate_access_variants_inputs_cmo \
---output_file_name ${small_variants_inputs} \
---project_name ${PROJECT_ID} \
---default_normal_path ${DEFAULT_DUPLEX_NORMAL_PATH} \
---tumor_bams_directory ${DUPLEX_TUMOR_BAMS_DIRECTORY} \
---normal_bams_directory ${UNFILTERED_NORMAL_BAMS_DIRECTORY} \
---simplex_bams_directory ${SIMPLEX_TUMOR_BAMS_DIRECTORY} \
---curated_bams_duplex_directory ${CURATED_DUPLEX_BAMS_DIRECTORY} \
---curated_bams_simplex_directory ${CURATED_SIMPLEX_BAMS_DIRECTORY} \
---pairing_file_path ${PAIRING_FILE_PATH} \
---default_stdnormal_path ${DEFAULT_STANDARD_NORMAL_PATH} \
---standard_bams_directory ${STANDARD_TUMOR_BAMS_DIRECTORY}
+--output_file_name "${small_variants_inputs}" \
+--project_name "${PROJECT_ID}" \
+--default_normal_path "${DEFAULT_DUPLEX_NORMAL_PATH}" \
+--tumor_bams_directory "${DUPLEX_TUMOR_BAMS_DIRECTORY}" \
+--normal_bams_directory "${UNFILTERED_NORMAL_BAMS_DIRECTORY}" \
+--simplex_bams_directory "${SIMPLEX_TUMOR_BAMS_DIRECTORY}" \
+--curated_bams_duplex_directory "${CURATED_DUPLEX_BAMS_DIRECTORY}" \
+--curated_bams_simplex_directory "${CURATED_SIMPLEX_BAMS_DIRECTORY}" \
+--pairing_file_path "${PAIRING_FILE_PATH}" \
+--default_stdnormal_path "${DEFAULT_STANDARD_NORMAL_PATH}" \
+--standard_bams_directory "${STANDARD_TUMOR_BAMS_DIRECTORY}"
 
 cd copy_number_variants
 generate_copynumber_inputs \
---project_id ${PROJECT_ID} \
---output_file_name ${cnv_inputs} \
---title_file_path ${TITLE_FILE_PATH} \
---tumor_bams_directory ${UNFILTERED_TUMOR_BAMS_DIRECTORY} \
+--project_id "${PROJECT_ID}" \
+--output_file_name "${cnv_inputs}" \
+--title_file_path "${TITLE_FILE_PATH}" \
+--tumor_bams_directory "${UNFILTERED_TUMOR_BAMS_DIRECTORY}" \
 --output_directory . \
 --tmp_dir /scratch \
 -alone
 cd ..
 
 generate_msi_inputs \
---project_name ${PROJECT_ID} \
---output_file_name ${msi_inputs} \
---standard_bams_directory ${STANDARD_TUMOR_BAMS_DIRECTORY} \
+--project_name "${PROJECT_ID}" \
+--output_file_name "${msi_inputs}" \
+--standard_bams_directory "${STANDARD_TUMOR_BAMS_DIRECTORY}" \
 --output_directory ./microsatellite_instability \
 --tmp_dir /scratch \
 -alone
@@ -146,7 +140,7 @@ msi_workflow=/work/access/production/workflows/access_workflows/v1/pipeline_1.3.
 
 pipeline_submit \
 --output_location small_variants \
---inputs_file ./${small_variants_inputs} \
+--inputs_file ./"${small_variants_inputs}" \
 --workflow ${small_variants_workflow} \
 --batch_system lsf \
 --leader_queue general \
@@ -154,7 +148,7 @@ pipeline_submit \
 
 pipeline_submit \
 --output_location structural_variants \
---inputs_file ./${small_variants_inputs} \
+--inputs_file ./"${small_variants_inputs}" \
 --workflow ${structural_variants_workflow} \
 --batch_system lsf \
 --leader_queue general \
@@ -163,7 +157,7 @@ pipeline_submit \
 cd copy_number_variants
 pipeline_submit \
 --output_location copy_number_variants \
---inputs_file ./${cnv_inputs} \
+--inputs_file ./"${cnv_inputs}" \
 --workflow ${cnv_workflow} \
 --batch_system lsf \
 --leader_queue general \
@@ -172,7 +166,7 @@ cd ..
 
 pipeline_submit \
 --output_location microsatellite_instability \
---inputs_file ${msi_inputs} \
+--inputs_file "${msi_inputs}" \
 --workflow ${msi_workflow} \
 --batch_system lsf \
 --leader_queue general \
