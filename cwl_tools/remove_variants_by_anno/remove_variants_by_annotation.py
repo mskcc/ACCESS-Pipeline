d88dcb9ded41fecf6d432799e9db73d34f17098b
andurill <gowtham.jn@gmail.com>
2020-07-09 09:28:21 -0400

    Update to MET exon14 skipping variant detection

--- cwl_tools/remove_variants_by_anno/remove_variants_by_annotation.py
+++ cwl_tools/remove_variants_by_anno/remove_variants_by_annotation.py
@@ -57,8 +57,8 @@ def filter_by_annotation(args):
     df_input = pd.read_csv(args.input_maf,sep='\t', header=1)
     keep_exonic = ['Missense_Mutation', 'Nonsense_Mutation', 'Splice_Site', 'Frame_Shift_Ins', 'Frame_Shift_Del', 'In_Frame_Ins', 'In_Frame_Del', 'Translation_Start_Site', 'Nonstop_Mutation', 'Silent']
 
-    Bool_exon = df_input['Variant_Classification'].isin(keep_exonic)   
-    Bool_MET = (df_input['Variant_Classification']=='Intron') & (df_input['Hugo_Symbol']=='MET')
+    Bool_exon = df_input['Variant_Classification'].isin(keep_exonic)
+    Bool_MET = (df_input['Variant_Classification'].isin([""])'Splice_Region') & (df_input['Hugo_Symbol']=='MET') & (((df_input["Start_Position"].apply(int) >= 116411903-100) & (df_input["Start_Position"].apply(int) <= 116412043+100)) | ((df_input["End_Position"].apply(int) >= 116411903-100) & (df_input["End_Position"].apply(int) <= 116412043+100)))
     Bool_TERT = (df_input['Variant_Classification']=="5'Flank") & (df_input['Hugo_Symbol']== 'TERT')
     #Removes based on Annotations
     df_kept = df_input[Bool_exon|Bool_MET|Bool_TERT]
